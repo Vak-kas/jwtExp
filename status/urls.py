29a796868e27cafb3b55ba608e3938160b541ad6
서민재 <hey_minj@naver.com>
2024-11-19 15:36:17 +0900

    상세정보

--- status/urls.py
+++ status/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # URL 패턴 추가 예시
     path('', index, name='index'),  # 기본 경로 설정 (필요에 따라 수정)
     path('recorded-sessions/', get_recorded_sessions, name='recorded-sessions'),
+    path('recorded-session/<int:session_id>/', get_recorded_session_detail, name='recorded_session_detail'),
 ]
--- status/views.py
+++ status/views.py
@@ -12,3 +12,16 @@ def get_recorded_sessions(request):
     sessions = RecordedSession.objects.all().values('id', 'session_name', 'start_time', 'end_time')
     return JsonResponse({'sessions': list(sessions)})
 
+
+
+def get_recorded_session_detail(request, session_id):
+    try:
+        session = RecordedSession.objects.get(id=session_id)
+        return JsonResponse({
+            'session_name': session.session_name,
+            'start_time': session.start_time,
+            'end_time': session.end_time,
+            'data': session.data
+        })
+    except RecordedSession.DoesNotExist:
+        return JsonResponse({'error': 'Session not found'}, status=404)
